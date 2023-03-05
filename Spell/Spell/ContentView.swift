3d448ad851a3e061209ed31099e97dee05fb8ea6
Andre Pham <andrepham33@gmail.com>
2023-03-06 10:32:54 +1100

    Created new project

---
+++ Spell/Spell/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spell
+//
+//  Created by Andre Pham on 6/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spell/Spell/SpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellApp.swift
+//  Spell
+//
+//  Created by Andre Pham on 6/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
